7f6499409e316a677f20228a012b2b8cab5071bd
Mercy1322 <163237375+Mercy1322@users.noreply.github.com>
2025-05-21 02:26:13 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,20 +2,6 @@ import sys
 
 from fastapi import FastAPI
 
-description = """
-# Bookstore API
-
-Bookstore application allows you to do basic querys in a Sqlite database
-with two tables Authors and Books.
-
-## Operations
-
-List, create, update and delete books and authors.
-
-One author can be linked to only one book.
-
-Books can be listed and filtered by author_id
-"""
 
 app = FastAPI(
     title="Bookstore REST Api",
