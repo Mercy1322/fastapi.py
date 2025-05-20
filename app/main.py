3c65aaf2bce567290551f8c4f8514c10914c0f00
Mercy1322 <163237375+Mercy1322@users.noreply.github.com>
2025-05-21 02:26:42 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,10 +9,5 @@ app = FastAPI(
     summary="CRUD of authors and books",
     version="1.0.0",
     terms_of_service="",
-    contact={
-        "name": "Angela Checa",
-        "url": "https://github.com/Trjegul84",
-        "email": "angela@gmail.com",
-    },
     license_info="",
 )
